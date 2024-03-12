9db05eab9fb7b253ed4e77604d4d9dd93e53e845
Lukas Vierling <lukas.vierling@rwth-aachen.de>
2024-03-12 17:23:33 +0800

    put model on gpu lol (forgot about that)

--- swarm/optimizer/edge_optimizer/edge_network.py
+++ swarm/optimizer/edge_optimizer/edge_network.py
@@ -13,6 +13,8 @@ from swarm.graph.composite_graph import CompositeGraph
 
 class EdgeNetwork(nn.Module):
     def __init__(self, llm_backbone_name, num_edges, initial_probability=0.5):
+        #if cude available set self.device to cuda
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         super(EdgeNetwork, self).__init__()
         self.llm_backbone = AutoModel.from_pretrained(llm_backbone_name)
         self.tokenizer = AutoTokenizer.from_pretrained(llm_backbone_name)
@@ -22,27 +24,20 @@ class EdgeNetwork(nn.Module):
         #freeze the backbone
         for param in self.llm_backbone.parameters():
             param.requires_grad = False
-        # check if tokenizer is the same as for the data preprocessing (eos_token, etc.)
-        # Checked: Seems as if the input is basic text so the tokenizer can stay because text is not tokenized yet
         self.linear = nn.Linear(self.llm_backbone.config.hidden_size, num_edges)
         nn.init.zeros_(self.linear.weight)
-        # fill bias so that we get the initial probability after applying sigmoid
-        # fill weights with zero
         bias = torch.full((num_edges,), torch.log(torch.tensor(initial_probability / (1 - initial_probability))))
         with torch.no_grad():
             self.linear.bias.copy_(bias)
-        #self.sigmoid = nn.Sigmoid() #No Sigmoid because we do that later
+        # Move model to GPU
+        self.to(self.device)
 
     def forward(self, input_text):
-        
-        input_ids = self.tokenizer.batch_encode_plus(input_text, padding=True, truncation=True, return_tensors='pt')['input_ids']
-        # GEt the hidden states
+        # Move input to GPU
+        input_ids = self.tokenizer.batch_encode_plus(input_text, padding=True, truncation=True, return_tensors='pt')['input_ids'].to(self.device)
         llm_bare_output = self.llm_backbone(input_ids)
         llm_output = llm_bare_output.last_hidden_state
-        # get hidden state of last token
         llm_output = llm_output[0][-1]
-        # get edge logtis
         edge_logits = self.linear(llm_output)
-        #edge_probs = self.sigmoid(edge_logits)
-        return edge_logits
+        return edge_logits.cpu()
     
